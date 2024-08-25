101bc47e77a3050573be8ef72ecdfeca5153885b
iman <dev.imansabet@gmail.com>
2024-08-25 12:23:13 +0330

    add UserSession - LocalStorageService & Serializations

---
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,23 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ClientLibrary.Services.Contracts;
+
+namespace ClientLibrary.Services.Implementations;
+
+public class UserAccountService : IUserAccountService
+{
+    public Task<GeneralResponse> CreateAsync(Register user)
+    {
+
+    }
+
+    public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
+    {
+
+    }
+
+    public Task<LoginResponse> SignInAsync(Login user)
+    {
+
+    }
+}
