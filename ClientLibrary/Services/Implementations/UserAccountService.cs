b0009d07f6b4155b12b203b436f56a474e4ee455
iman <dev.imansabet@gmail.com>
2024-08-25 12:56:59 +0330

    inject GetHttpClient in User Acoount service - implement Create method (register)

--- ClientLibrary/Services/Implementations/UserAccountService.cs
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -1,14 +1,23 @@
 ﻿using BaseLibrary.DTOs;
 using BaseLibrary.Responses;
+using ClientLibrary.Helpers;
 using ClientLibrary.Services.Contracts;
+using System.Net.Http.Json;
 
 namespace ClientLibrary.Services.Implementations;
 
-public class UserAccountService : IUserAccountService
+public class UserAccountService(GetHttpClient getHttpClient) : IUserAccountService
 {
-    public Task<GeneralResponse> CreateAsync(Register user)
+    public const string AuthUrl = "api/authentication";
+    public async Task<GeneralResponse> CreateAsync(Register user)
     {
+        var httpClient = getHttpClient.GetPublicHttpClient();
+        
+        var result = await httpClient.PostAsJsonAsync($"{AuthUrl}/register", user);
 
+        if (!result.IsSuccessStatusCode) return new GeneralResponse(false, "Error occured");
+
+        return await result.Content.ReadFromJsonAsync<GeneralResponse>()!;
     }
 
     public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
