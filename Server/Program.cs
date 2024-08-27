27e1ff1114238e5897f3add0dc20c558b87a7dd3
iman <dev.imansabet@gmail.com>
2024-08-27 20:17:42 +0330

    setup CORS (Cross-Origin Resource Sharing) for Blazor request

--- Server/Program.cs
+++ Server/Program.cs
@@ -22,6 +22,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
 builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+    "AllowBlazorWasm",
+    builder => builder.WithOrigins("https://localhost:7119","http://localhost:5107")
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+
+    ); ;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
