b3271655bf119d4442654b277d4c844bcc51f99c
iman <dev.imansabet@gmail.com>
2024-09-20 15:55:12 +0330

    Add AddorUpdateEmployeePage

--- Server/Program.cs
+++ Server/Program.cs
@@ -46,13 +46,16 @@ builder.Services.AddAuthentication(options =>
 });
 
 builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
-
+////////////////////////////////
 builder.Services.AddScoped<IGenericRepositoryInterface<GeneralDepartment>,GeneralDepartmentRepository >();
 builder.Services.AddScoped<IGenericRepositoryInterface<Department>, DepartmentRepository>();
 builder.Services.AddScoped<IGenericRepositoryInterface<Branch>,BranchRepository >();
+///////////////////////////
 builder.Services.AddScoped<IGenericRepositoryInterface<Country>, CountryRepository>();
 builder.Services.AddScoped<IGenericRepositoryInterface<City>,CityRepository >();
 builder.Services.AddScoped<IGenericRepositoryInterface<Town>, TownRepository>();
+//////////////////////////
+builder.Services.AddScoped<IGenericRepositoryInterface<Employee>, EmployeeRepository>();
 
 
 
